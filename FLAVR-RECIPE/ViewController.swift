cb5b0da3f49cfeb8bd4e6094999c92e1a5170350
Renzo Alvaroshan <renzo.alvaroshan@gmail.com>
2022-03-31 16:22:01 +0700

    Initial Commit

---
+++ FLAVR-RECIPE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FLAVR-RECIPE
+//
+//  Created by Renzo Alvaroshan on 31/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
